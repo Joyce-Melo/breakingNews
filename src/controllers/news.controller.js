b79e88c953819b4ed0f1406d0a6e05dff4b5973b
Joyce Melo <joyce.santosm@hotmail.com>
2023-02-20 21:00:14 -0300

    feat: middleware validation finished

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -3,27 +3,6 @@ import { createService, findAllService } from "../services/news.service.js"
 
 const create = async (req, res) => {
     try{ //Como faço para pegar o id do user no login? No auth.service usamos o id como um dos componenetes para criar o token, e ao final do login no auth.controller nós retornamos o token, então para sabermos qual id está conectado nós utilizamos esse token que foi retornado
-
-        const { authorization } = req.headers;
-
-        if(!authorization){
-            return res.send(401);
-        }
-
-        const parts = authorization.split(" ") //Separando o que está dentro do authorization por espaço, pois assim conseguimos verificar se há a palavra bearer que é obrigatória
-        
-        if(parts.lenght !== 2 ){ //nosso parts sempre será 2, caso não seja 2 está errado
-            return res.send(401);
-        }
-
-        const [schema, token] = parts //desestruturando um array (nosso authorization, lembrando que uma string é um array de caracteres)
-
-        if(schema !== "Bearer"){ //verificando se a primeira posição de authorization é o bearer
-            returnres.send(401);
-        }
-
-
-        
         const {title, text, banner} = req.body;
 
         if(!title || !banner || !text) {
@@ -36,8 +15,7 @@ const create = async (req, res) => {
             title,
             text,
             banner,
-            user: {_id: '63f2a89c39993881ce8c69af'},
-
+            user: req.userId, //agora conseguimos mandar o que vem do decoded id lá do middleware
         })
 
         res.send(201);
