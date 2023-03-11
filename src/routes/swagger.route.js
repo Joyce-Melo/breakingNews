3697fdca87110274c9b4837b1a5a3f791faa0fc0
Joyce Melo <joyce.santosm@hotmail.com>
2023-03-11 11:38:33 -0300

    refactor: comment removed

--- src/routes/swagger.route.js
+++ src/routes/swagger.route.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 const router = Router();
 
-import swaggerUi from "swagger-ui-express"
-import swaggerDocument from "../swagger.json" assert {type: "json"}; //esse é um arquivo que eu criei na raiz do nosso projeto, e lá colocaremos as expecificações do nosso projeto e o swagger ui irá rodar isso através de uma interface gráfica
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json" assert { type: "json" };
 
 //precisamos puxar o swagger lá no index tb
 router.use("/", swaggerUi.serve); //estou falando para a rota utilizar isso aqui, cria um servidorzinho para o swagger
